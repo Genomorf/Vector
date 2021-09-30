f4be0f46ad65413adf0664c8446673a3cc4663db
gena <blackkolya1234567890@yandex.ru>
2021-09-30 21:59:31 +0300

    moved Vector.cpp to Vector.h, created main.cpp with helpers and tests, added full random_iterators, emplace back and other small edits

--- src/Vector.h
+++ src/Vector.h
@@ -1,32 +1,17 @@
+#pragma once
+
 #include <iostream>
 #include <initializer_list>
 #include <vector>
 #include <algorithm>
-//using namespace std;
+#include <type_traits>
+
 using std::cout;
 using std::swap;
 using std::move;
 
 static const int max_reverse_value = 100'000;
 static const int max_resize_value = 100'000;
-
-
-template<typename T>
-std::ostream& operator<<(std::ostream& out, std::vector<T>& vec) {
-	for (const auto& i : vec) {
-		out << i;
-		if (i != vec.back())
-			out << " ";
-	}
-	out << std::endl;
-	return out;
-}
-
-template <typename T>
-class VectorBuffer;
-
-template <typename T>
-void swap(VectorBuffer<T>& lhs, VectorBuffer<T>& rhs);
 
 template<typename T>
 class VectorBuffer {
@@ -37,7 +22,7 @@ public:
 
 	VectorBuffer(size_t capacity = 0, size_t size = 0) : capacity_(capacity), size_(size) {
 		void* new_buff = ::operator new(sizeof(T) * capacity);
-		buff = reinterpret_cast<T*>(new_buff);
+		buff = static_cast<T*>(new_buff);
 		for (size_t i = 0; i < size; i++)
 		{
 			new (buff + i) T();
@@ -78,11 +63,12 @@ public:
 			buff[size_ - 1 - i].~T();
 		}
 	}
-
+	
 	~VectorBuffer() {
 		destructElements();
 		::operator delete(buff);
 	}
+	void swap(VectorBuffer<T>& lhs, VectorBuffer<T>& rhs);
 };
 
 template <typename T>
@@ -91,7 +77,6 @@ void swap(VectorBuffer<T>& lhs, VectorBuffer<T>& rhs) {
 	swap(lhs.capacity_, rhs.capacity_);
 	swap(lhs.size_, rhs.size_);
 }
-
 
 template <typename T>
 typename std::enable_if<std::is_nothrow_move_constructible<T>::value>::type
@@ -105,42 +90,50 @@ move_if_noexcept_else_copy(T* src, T* dst, int size) {
 	std::copy(src, src + size, dst);
 }
 
+// TODO const??
 template <typename T>
 class Vector {
 public:
 	VectorBuffer<T> buff;
 	Vector(const std::initializer_list<T>& in_list) {
-		VectorBuffer<T> tmpbuff(in_list.size(), in_list.size());
-		size_t counter = 0;
-		for (auto it = in_list.begin(); it != in_list.end(); ++it, ++counter)
+		VectorBuffer<T> tmpbuff(in_list.size(), 0);
+		auto it = in_list.begin();
+		for (size_t i = 0; i < in_list.size(); i++)
 		{
-			tmpbuff[counter] = *it; // from std::initializer_list can only copy objects
+			new (tmpbuff.buff + i) T(it[i]);
 		}
+		tmpbuff.size_ = in_list.size();
 		swap(tmpbuff, buff);
 	}
 	Vector(size_t amount = 0, const T& value = T()) {
-		VectorBuffer<T> tmpbuff(amount, amount);
-		for (size_t i = 0; i < tmpbuff.size_; ++i)
+		VectorBuffer<T> tmpbuff(amount, 0);
+		for (size_t i = 0; i < amount; ++i)
 		{
-			tmpbuff[i] = value;
+			new (tmpbuff.buff + i) T(value);
 		}
+		tmpbuff.size_ = amount;
+		swap(tmpbuff, buff);
+	}
+	template<typename Iter, typename = typename std::enable_if<!std::is_integral<Iter>::value>::type>
+	Vector(Iter first, Iter last) {
+		auto size = std::distance(first, last);
+		VectorBuffer<T> tmpbuff(size, 0);
+		size_t i = 0;
+		for (; first != last; ++first, ++i) {
+			new (tmpbuff.buff + i) T(*first);
+		}
+		tmpbuff.size_ = size;
 		swap(tmpbuff, buff);
 	}
 	T& operator[] (size_t n) {
 		return buff[n];
 	}
-	//void copyToBuff(VectorBuffer<T>& otherbuff) {
-	//	for (size_t i = 0; i < buff.size_; i++)
-	//	{
-	//		otherbuff[i] = std::move_if_noexcept(buff[i]);
-	//	}
-	//}
 	void reserve(size_t new_capacity) {
 		if (new_capacity < buff.capacity_ || new_capacity >= max_reverse_value) return;
 		VectorBuffer<T> tmpbuff(new_capacity, 0);
 		for (size_t i = 0; i < buff.size_; i++)
 		{
-			// don't construct values by default, placeemnt new right in memory
+			// don't construct values by default, placement new right in memory
 			new (tmpbuff.buff + i) T(std::move_if_noexcept(buff[i])); 
 		}
 		tmpbuff.size_ = buff.size_;
@@ -158,9 +151,7 @@ public:
 			swap(tmpbuff, buff);
 		}
 	}
-	// TODO:
 	void pushBack(const T& value) {
-		cout << "1";
 		if (buff.capacity_ == buff.size_) {
 			buff.capacity_ = buff.capacity_ * 2;
 		}
@@ -171,7 +162,6 @@ public:
 		++buff.size_;
 	}
 	void pushBack(T&& value) {
-		cout << "2";
 		if (buff.capacity_ == buff.size_) {
 			buff.capacity_ = buff.capacity_ * 2;
 		}
@@ -181,133 +171,66 @@ public:
 		swap(tmpbuff, buff);
 		++buff.size_;
 	}
-	/*
-		template<typename T>
-	void emplaceBack(T&& t) {
+	template <typename ... Args>
+	void emplaceBack(Args&&... args) {
 		if (buff.capacity_ == buff.size_) {
-			MyVectorBuff tmpbuff(buff.size_ * 2, buff.size_);
-			// TODO: if moveable - move, else copy
-			buff.copyFromBuffToOtherBuff(tmpbuff);
-			swap(tmpbuff, buff);
+			buff.capacity_ = buff.capacity_ * 2;
 		}
-		buff[buff.size_] = forward<T>(t);
+		VectorBuffer<T> tmpbuff(buff.capacity_, buff.size_);
+		move_if_noexcept_else_copy(buff.buff, tmpbuff.buff, buff.size_);
+		new (&tmpbuff.buff[buff.size_]) T(std::forward<Args>(args)...);
+		swap(tmpbuff, buff);
 		++buff.size_;
 	}
-	*/
+	//template<typename Iter>
+
+	//template<typename Iter>
+	//void insert(Iter pos, T&& value) {
+
+	//}
 	class Iterator {
 	public:
+		using value_type = T;
+		using pointer = T*;
+		using reference = T&;
+		using difference_type = std::ptrdiff_t;
+		using iterator_category = std::random_access_iterator_tag;
+
 		Vector<T>* sv = nullptr;
 		int i = 0;
 		Iterator(Vector<T>* sv, int i) : sv(sv), i(i) {}
-		T& operator*() { return (*sv)[i]; }
+		reference operator*() { return (*sv)[i]; }
+		pointer operator->() { return &((*sv)[i]); }
+		reference operator[] (int n) { return (*sv)[i + n]; }
 		Iterator& operator++() { ++i; return *this; }
-		//Iterator operator++(int) { Iterator tmp(*this); ++ptr; return tmp; }
-		friend bool operator== (const Iterator& lhs, const Iterator& rhs) { return lhs.i == rhs.i; }
-		friend bool operator!= (const Iterator& lhs, const Iterator& rhs) { return lhs.i != rhs.i; }
+		Iterator& operator--() { --i; return *this; }
+		Iterator operator++(int) { Iterator tmp(*this); ++i; return tmp; }
+		Iterator operator--(int) { Iterator tmp(*this); --i; return tmp; }
+		Iterator& operator+=(int n) { i += n; return *this; }
+		Iterator& operator-=(int n) { i -= n; return *this; }
+		Iterator operator+ (int n) { Iterator tmp(*this); return tmp += n; }
+		Iterator operator- (int n) { Iterator tmp(*this); return tmp -= n; }
+		difference_type operator- (const Iterator& other) { return i - other.i; }
+		bool operator<(const Iterator& other)  const {return i < other.i;}
+		bool operator<=(const Iterator& other) const {return i <= other.i;}
+		bool operator>(const Iterator& other)  const {return i > other.i;}
+		bool operator>=(const Iterator& other) const {return i >= other.i;}
+		bool operator!=(const Iterator & other) const {return i != other.i;}
+		bool operator==(const Iterator & other) const {return i == other.i;}
 	};
 	Iterator begin() { return Iterator(this, 0); }
 	Iterator end() { return Iterator(this, size()); }
 	size_t capacity() const { return buff.capacity_; }
 	size_t size() const { return buff.size_; }
-};
 
-
-struct MyInt {
-	int* x_ = nullptr;
-	MyInt() {
-		cout << "constructed\n";
-		x_ = new int(10);
-	}
-	MyInt(int x) {
-		x_ = new int(x);
-	}
-	MyInt(const MyInt& mi) {
-		x_ = new int(*mi.x_);
-		cout << "copied\n";
-	}
-	MyInt& operator=(const MyInt& mi) {
-		x_ = new int(*mi.x_);
-		cout << "copied =\n";
-		return *this;
-	}
-	MyInt(MyInt&& mi) noexcept {
-		swap(*this, mi);
-		cout << "moved\n";
-	}
-	MyInt& operator=(MyInt&& mi)noexcept {
-		swap(*this, mi);
-		cout << "moved =\n";
-		return *this;
-	}
-	~MyInt() {
-		cout << "destructed\n";
-		delete x_;
-	}
-	friend void swap(MyInt& lhs, MyInt& rhs);
-};
-void swap(MyInt& lhs, MyInt& rhs) {
-	swap(lhs.x_, rhs.x_);
-}
-struct MyIntWithNoDefaultConstr {
-	int x = 10;
-	MyIntWithNoDefaultConstr() = delete;
-};
-std::ostream& operator <<(std::ostream& out, const MyInt& mi) {
-	out << *mi.x_ << " ";
-	return out;
-}
-#include <type_traits>
-template <typename T>
-void printVector(Vector<T>& sv) {
-	static int count = 0;
-	++count;
-	cout << count <<  " Capacity: " << sv.capacity() << " Size: " << sv.size() << '\n';
-
-	for (auto it = sv.begin(), it_end = sv.end(); it != it_end; ++it) {
-		cout << *it << " ";
-	}
-	cout << '\n';
-
-}
-
-
-
-
-
-
-
-
-//template <typename Iter, typename T>
-//typename std::enable_if<!std::is_nothrow_move_constructible<T>::value>::type
-//copyFromInitList(Iter first, Iter last, T* dst) {
-//	std::copy(first, last, dst);
-//}
-//
-//template <typename Iter, typename T>
-//typename std::enable_if<std::is_nothrow_move_constructible<T>::value>::type
-//copyFromInitList(Iter first, Iter last, T* dst) {
-//	std::move(first, last, dst);
-//}
-
-int main() {
-	Vector<MyInt> v{ 1,2,3 };
-	printVector(v);
-	MyInt x = 1;
-	//MyInt x2 = move(x);
-	v.pushBack(MyInt(1));
-	printVector(v);
-
-	//copy_if_moveable(&il, x, 3);
-	//for (size_t i = 0; i < 3; i++)
-	//{
-	//	x[i] = i;
+	//void insert(typename Vector<T>::Iterator pos, const T& value) {
+	//	if (buff.capacity_ == buff.size_) {
+	//		buff.capacity_ = buff.capacity_ * 2;
+	//	}
+	//	VectorBuffer<T> tmpbuff(buff.capacity_, buff.size_);
+	//	auto dist = std::distance(this->begin(), pos);
+	//	cout << dist << '\n';
 	//}
-	//MyInt* x2 = new MyInt[3];
-	//std::move(x, x + 3, x2);
-	//sv.resize(20);
-	//printVector(sv);
-	//printVectorBuffer(sv);
-	//VectorBuffer<MyInt> sv2(move(sv));
-	//printVectorBuffer(sv2);
-	//printVectorBuffer(sv);
-}
+};
+
+
