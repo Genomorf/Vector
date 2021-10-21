75185a341371e5d7beeb0516536efef12e9cf942
gena <blackkolya1234567890@yandex.ru>
2021-10-21 14:31:03 +0300

    refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,10 @@
 #include "helpers.h"
-#include <chrono>
+
 int main() {
-	//Vector<MyInt> v;
-	//v.push_back(10);
-	//v.push_back(10);
-	//v.push_back(10);
-	//v.push_back(10);
-	//v.push_back(10);
-	//printVector(v);
 
 
-
-//#if 0
+// some tests and measurement 
+#if 0
 	int n;
 	std::vector<int> v;
 	std::cin >> n;
@@ -33,7 +26,7 @@ int main() {
 	auto finish2 = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> elapsed2 = finish2 - start2;
 	std::cout << "Elapsed time: " << elapsed2.count() << " s\n";
-#if 0
+
 	using Vector_mi = Vector<MyInt>;
 	Vector_mi v1{ 1,2,3 };
 	// initialisation
